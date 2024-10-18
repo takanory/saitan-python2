42e320338e12ce42a547fa9df3760419619bb9ba
安田善一郎 <zen@zenich.com>
2024-10-18 10:33:18 +0900

    実践編 8章 ユーザーが自分のデータを持てる

--- 実践編/chapter08/ユーザーが自分のデータを持てるようにする/accounts/views.py
+++ 実践編/chapter08/ユーザーが自分のデータを持てるようにする/accounts/views.py
@@ -4,20 +4,20 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 def register(request):
-    """Register a new user."""
+    """新しいユーザーを登録する"""
     if request.method != 'POST':
-        # Display blank registration form.
+        # 空のユーザー登録フォームを表示する
         form = UserCreationForm()
     else:
-        # Process completed form.
+        # 入力済みのフォームを処理する
         form = UserCreationForm(data=request.POST)
 
         if form.is_valid():
             new_user = form.save()
-            # Log the user in and then redirect to home page.
+            # ユーザーをログインさせてホームページにリダイレクトする
             login(request, new_user)
             return redirect('learning_logs:index')
 
-    # Display a blank or invalid form.
+    # 空または無効のフォームを表示する
     context = {'form': form}
     return render(request, 'registration/register.html', context)
--- 実践編/chapter08/ユーザーが自分のデータを持てるようにする/learning_logs/urls.py
+++ 実践編/chapter08/ユーザーが自分のデータを持てるようにする/learning_logs/urls.py
@@ -1,4 +1,4 @@
-"""Defines URL patterns for learning_logs."""
+"""learning_logsのURLパターンの定義"""
 
 from django.urls import path
 
@@ -6,16 +6,16 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    # Home page
+    # ホームページ
     path('', views.index, name='index'),
-    # Page that shows all topics.
+    # すべてのトピックを表示するページ
     path('topics/', views.topics, name='topics'),
-    # Detail page for a single topic.
+    # 個別トピックの詳細ページ
     path('topics/<int:topic_id>/', views.topic, name='topic'),
-    # Page for adding a new topic.
+    # 新規トピックの追加ページ
     path('new_topic/', views.new_topic, name='new_topic'),
-    # Page for adding a new entry.
-    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
-    # Page for editing an entry.
+    # 新規記事の追加ページ
+    path('new_entry/<int:topic_id>', views.new_entry, name='new_entry'),
+    # 記事の編集ページ
     path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
