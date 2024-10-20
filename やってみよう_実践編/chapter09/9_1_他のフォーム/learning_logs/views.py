d081c9ce7e177c94d89830cc669c68c89dae7bb5
安田善一郎 <zen@zenich.com>
2024-10-20 23:04:22 +0900

    WIP やってみよう実践編 9_1

--- やってみよう_実践編/chapter09/9_1_他のフォーム/learning_logs/views.py
+++ やってみよう_実践編/chapter09/9_1_他のフォーム/learning_logs/views.py
@@ -7,21 +7,21 @@ from .forms import TopicForm, EntryForm
 
 
 def index(request):
-    """The home page for Learning Log."""
+    """学習ノートのホームページ"""
     return render(request, 'learning_logs/index.html')
 
 @login_required
 def topics(request):
-    """Show all topics."""
+    """すべてのトピックを表示する"""
     topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
 @login_required
 def topic(request, topic_id):
-    """Show a single topic and all its entries."""
+    """1つのトピックとそれについてのすべての記事を表示"""
     topic = Topic.objects.get(id=topic_id)
-    # Make sure the topic belongs to the current user.
+    # トピックが現在のユーザーが所持するものであることを確認する
     if topic.owner != request.user:
         raise Http404
 
@@ -29,35 +29,35 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
-@login_required    
+@login_required
 def new_topic(request):
-    """Add a new topic."""
+    """新規トピックを追加する"""
     if request.method != 'POST':
-        # No data submitted; create a blank form.
+        # データは送信されていないので空のフォームを生成する
         form = TopicForm()
     else:
-        # POST data submitted; process data.
+        # POSTでデータが送信されたのでこれを処理する
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            new_topic = form.save(commit=False)
-            new_topic.owner = request.user
+            new_topic = form.save(commit=False) 
+            new_topic.owner = request.user 
             new_topic.save()
             return redirect('learning_logs:topics')
 
-    # Display a blank or invalid form.
+    # 空または無効のフォームを表示する
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
-@login_required    
+@login_required
 def new_entry(request, topic_id):
-    """Add a new entry for a particular topic."""
+    """特定のトピックに新規記事を追加する"""
     topic = Topic.objects.get(id=topic_id)
-    
+
     if request.method != 'POST':
-        # No data submitted; create a blank form.
+        # データは送信されていないので空のフォームを生成する
         form = EntryForm()
     else:
-        # POST data submitted; process data.
+        # POSTでデータが送信されたのでこれを処理する
         form = EntryForm(data=request.POST)
         if form.is_valid():
             new_entry = form.save(commit=False)
@@ -65,23 +65,23 @@ def new_entry(request, topic_id):
             new_entry.save()
             return redirect('learning_logs:topic', topic_id=topic_id)
 
-    # Display a blank or invalid form.
+    # 空または無効のフォームを表示する
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
 @login_required
 def edit_entry(request, entry_id):
-    """Edit an existing entry."""
+    """既存の記事を編集する"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
     if topic.owner != request.user:
         raise Http404
 
     if request.method != 'POST':
-        # Initial request; pre-fill form with the current entry.
+        # 初回リクエスト時は現在の記事の内容がフォームに埋め込まれている
         form = EntryForm(instance=entry)
     else:
-        # POST data submitted; process data.
+        # POSTでデータが送信されたのでこれを処理する
         form = EntryForm(instance=entry, data=request.POST)
         if form.is_valid():
             form.save()
